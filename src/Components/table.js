af9c3dd835e64743ecb1b5bdea6ac7eb3126603f
ngocsnake <ngocsnake@gmail.com>
2022-03-04 20:20:40 +0700

    fix no-mixed-operator

--- src/Components/table.js
+++ src/Components/table.js
@@ -18,28 +18,28 @@ function Table({ props }) {
                         setType({ column: 'name', desc: !type.desc })
                     }}
                     className="provincet">Tỉnh/Thành phố
-                    {(type.column !== 'name' && <FaSort></FaSort>) || Icon}
+                    {(type.column !== 'name' && <FaSort></FaSort>)}
                 </div>
                 <div
                     onClick={() => {
                         setType({ column: 'cases', desc: !type.desc })
                     }}
                     className="casest">Nhiễm
-                    {type.column !== 'cases' && <FaSort></FaSort> || Icon}
+                    {type.column !== 'cases' && <FaSort></FaSort>}
                 </div>
                 <div
                     onClick={() => {
                         setType({ column: 'casesToday', desc: !type.desc })
                     }}
                     className="todaycasest">Hôm nay
-                    {type.column !== 'casesToday' && <FaSort></FaSort> || Icon}
+                    {type.column !== 'casesToday' && <FaSort></FaSort>}
                 </div>
                 <div
                     onClick={() => {
                         setType({ column: 'death', desc: !type.desc })
                     }}
                     className="deatht">Tử vong
-                    {type.column !== 'death' && <FaSort></FaSort> || Icon}
+                    {type.column !== 'death' && <FaSort></FaSort>}
                 </div>
             </div>
         )
